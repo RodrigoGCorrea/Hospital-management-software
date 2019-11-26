47ca9d6ce29b3805136b8a81062be7febc06e650
Rodrigo Correa <rodrigogc@id.uff.br>
2019-11-25 21:47:30 -0300

    Room methods implemented

--- src/model/facilities/Room.java
+++ src/model/facilities/Room.java
@@ -1,4 +1,33 @@
 package model.facilities;
 
+import model.people.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    private int room;
+    private List<Patient> patientList = new ArrayList<>();
+
+    public Room(int room) {
+        this.room = room;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public boolean addPatient(Patient patient) {
+        for (Patient value : patientList) {
+            if (value.getCpf().equals(patient.getCpf())) {
+                return false;
+            }
+        }
+        patientList.add(patient);
+        return true;
+    }
 }
